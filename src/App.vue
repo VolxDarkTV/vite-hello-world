a2a1c7003f31ad7369a34ea0370624bbd66c5589
VolxDarkTV <vinvenzo.foti@outlook.it>
2022-12-07 22:18:03 +0100

    add title

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue'
+import AppTitle from './components/AppTitle.vue'
 </script>
 
 <template>
@@ -13,7 +14,8 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld/>
+  <AppTitle/>
 </template>
 
 <style scoped>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,13 @@
+<script>
+
+</script>
+
+<template>
+    <h1 id="title">La mia prima app con Vite!</h1>
+</template>
+
+<style>
+    #title{
+        background-color: tomato;
+    }
+</style>
